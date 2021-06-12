38e49ca592ea466ae020cfa9dad2234386f0ecbc
Gbadebo Bello <gbahdeybohbello@gmail.com>
2021-06-12 02:11:40 +0100

    Updated database url

--- mongo.js
+++ mongo.js
@@ -3,21 +3,11 @@ const mongoose = require('mongoose');
 const { config } = require('dotenv');
 config();
 
-let database_connection = process.env.database_connection;
-let db_username = process.env.db_username;
-let db_password = process.env.db_password;
+let database_url = process.env.database_url;
 
-console.log("database_connection iss => ", database_connection);
-console.log("db_username is =>", db_username)
-console.log("db_password is =>", db_password)
-// let url = "mongodb+srv://gbahdeyboh:password4password@cluster0.zpnwo.mongodb.net/KubernetesPracticeDB?retryWrites=true&w=majority";
-// password4password
+console.log("database_url is =>", database_url)
 
-const url = `mongodb://${db_username}:${db_password}@mongodb-service:27017/KubernetesPracticeDB`
-
-console.log("DB url is => ", url);
-
-mongoose.connect(url, {
+mongoose.connect(database_url, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
